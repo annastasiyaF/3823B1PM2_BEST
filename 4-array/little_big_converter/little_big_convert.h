4ea8a44dc5e73924443755b96056a9ed33d3d0b8
annastasiyaF <184filippova8a@mail.ru>
2023-11-29 13:06:45 +0300

    asd

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -1,5 +1,23 @@
 #include <math.h>
 
-unsigned int little_big_convert(unsigned int input){
-    return 0;
+int calc_percent(int a[], int size){
+    int max,min,c;
+    double r;
+    min=1000000;
+    max=-1000000;
+    c=0;
+    for (int i=0;i<size;i++){
+        if (a[i]>max)
+            max=a[i];
+    }
+    for (int i=0;i<size;i++){
+        if (a[i]<min)
+            min=a[i];
+    }
+    r=(fabs)((max-min))/2;
+    for (int i=0;i<size;i++){
+        if (a[i]>r)
+            c+=1;
+    }
+    return (c/(double)size)*100;
 }
